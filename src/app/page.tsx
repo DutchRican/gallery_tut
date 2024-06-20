954b07d5cf343b6459f686bd6945a202266421bd
Paul van Woensel <pvanwoensel@gmail.com>
2024-06-20 14:48:16 -0500

    only show images when logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,35 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic"
 
-export default async function HomePage() {
-
+async function Images() {
   const images = await db.query.images.findMany({ orderBy: (model, { desc }) => desc(model.id) });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map(({ id, name, url }) => (
+        <div key={id} className="flex w-48 flex-col">
+          <img
+            src={url}
+            alt={name}
+            className="object-cover"
+          />
+          <div className="text-end">{name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map(({ id, name, url }) => (
-          <div key={id} className="flex w-48 flex-col">
-            <img
-              src={url}
-              alt={name}
-              className="object-cover"
-            />
-            <div className="text-end">{name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center"> Please sign in above </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
