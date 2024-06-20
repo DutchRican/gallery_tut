c9c36f996c35f0ac20190b15f300f4e3ae8afdc0
Paul van Woensel <pvanwoensel@gmail.com>
2024-06-20 14:26:18 -0500

    added auth

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,13 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+export function TopNav() {
+	return (
+		<nav className="flex justify-between items-center w-full p-4 text-xl font-semibold border-b">
+			<div>Gallery</div>
+
+			<div>
+				<SignedOut> <SignInButton /> </SignedOut>
+				<SignedIn> <UserButton /> </SignedIn>
+			</div>
+		</nav>
+	);
+}
